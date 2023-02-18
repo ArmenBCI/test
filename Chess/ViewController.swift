148b7f1d950120158853bdb6f1c4aafa910ed62d
ArmenBCI <125582643+ArmenBCI@users.noreply.github.com>
2023-02-18 13:52:03 +0400

    Chess folders and files

---
+++ Chess/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  Chess
+//
+//  Created by Armen Sukiasyan on 02.02.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var startGame: UIButton!
+    
+    
+    
+    @IBOutlet var bordButtons: [UIButton]!
+    
+    
+    
+    @IBOutlet weak var figur: UIButton!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func actionStartButton(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func actionFigurButton(_ sender: UIButton) {
+    }
+    
+    
+    
+    
+    @IBAction func actionBoard(_ sender: UIButton) {
+    }
+    
+    
+    
+    
+    
+    
+}
+
